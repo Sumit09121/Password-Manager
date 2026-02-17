1227a17c261825030f6f2be437d4f948a5297a30
Sumit Lakhera <sumit.lakhera09@gmail.com>
2026-02-17 11:38:10 +0530

    fix component import case issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import Manager from './components/manager'
+import Manager from './components/Manager'
 import Footer from './components/Footer'
 
 function App() {
